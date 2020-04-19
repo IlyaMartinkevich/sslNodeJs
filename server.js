66937127a13dbab226d3a4db6bb250c8c23f10f4
Ilya Martinkevich <ilia1mail2martinkevi4@gmail.com>
2020-04-20 00:51:34 +0300

    add params in get request

--- server.js
+++ server.js
@@ -7,16 +7,17 @@ const morgan = require("morgan");
 app.use(morgan("dev"));
 const bodyParser = require('body-parser');
 
-
+var md5 = require('md5');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 
 //app.get('/getHashString/:str', function (req, res) {
-app.get('/', function (req, res) {
-   // res.statusCode=200;
-res.send("<h1>Hello World</h1>");
+app.get('/:str', function (req, res) {
+    var timeInMs = Date.now();
+
+res.send(md5(req.params.str+timeInMs));
 });
 
 http.createServer( app)
